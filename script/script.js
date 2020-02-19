cb1172003a7d8109c5c727f3724f3b5b004a3593
Sizon <sizon@vidrada.org>
2020-02-19 12:21:43 +0200

    fix global bugs

--- script/script.js
+++ script/script.js
@@ -1,4 +1,22 @@
-// console.dir(startButton);
+'use strict';
+
+const DATA = {
+  whichSite: ['landing', 'multiPage', 'onlineStore'],
+  price: [4000, 8000, 26000],
+  desktopTemplates: [50, 40, 30],
+  adapt: 20,
+  mobileTemplates: 15,
+  editable: 10,
+  metrikaYandex: [500, 1000, 2000],
+  analyticsGoogle: [850, 1350, 3000],
+  sendOrder: 500,
+  deadlineDay: [
+    [2, 7],
+    [3, 10],
+    [7, 14]
+  ],
+  deadlinePercent: [20, 17, 15]
+};
 
 const startButton = document.querySelector('.start-button'),
   firstScreen = document.querySelector('.first-screen'),
@@ -6,18 +24,40 @@ const startButton = document.querySelector('.start-button'),
   formCalculate = document.querySelector('.form-calculate'),
   endButton = document.querySelector('.end-button'),
   total = document.querySelector('.total'),
-  fastRange = document.querySelector('.fast-range');
+  fastRange = document.querySelector('.fast-range'),
+  totalPriseSum = document.querySelector('.total_price__sum');
 
-
-
-
+// Begin Function
 function showElem(elem) {
   elem.style.display = 'block';
 }
 
-
 function hideElem(elem) {
   elem.style.display = 'none';
+}
+
+function priceCalculation(elem) {
+  let result = 0,
+    index = 0;
+
+  if (elem.name === 'whichSite') {
+    for (const item of formCalculate.elements) {
+      if (item.type === 'checkbox') {
+        item.checked = false;
+      }
+    }
+    hideElem(fastRange);
+  }
+
+  for (const item of formCalculate.elements) {
+    if (item.name === 'whichSite' && elem.checked) {
+      index = DATA.whichSite.indexOf(item.value);
+      console.log(index);
+    }
+  }
+  result = DATA.price[index];
+
+  totalPriseSum.textContent = result;
 }
 
 function handlerCallbackForm(event) {
@@ -25,9 +65,13 @@ function handlerCallbackForm(event) {
 
   if (target.classList.contains('want-faster')) {
     target.checked ? showElem(fastRange) : hideElem(fastRange);
+  }
 
+  if (target.classList.contains('calc-handler')) {
+    priceCalculation(target);
   }
 }
+// End Function
 
 startButton.addEventListener('click', function () {
   showElem(mainForm);
