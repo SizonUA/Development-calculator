a3fa49fe4388ad45b501bdabcdf93f48601a3a57
Sizon <sizon@vidrada.org>
2020-02-20 19:22:46 +0200

    homework #1

--- script/script.js
+++ script/script.js
@@ -1,14 +1,14 @@
-'use strict';
+"use strict";
 
 const DATA = {
-  whichSite: ['landing', 'multiPage', 'onlineStore'],
-  price: [4000, 8000, 26000],
+  whichSite: ["landing", "multiPage", "onlineStore"],
+  price: [1000, 2000, 6000],
   desktopTemplates: [50, 40, 30],
   adapt: 20,
   mobileTemplates: 15,
   editable: 10,
-  metrikaYandex: [500, 1000, 2000],
-  analyticsGoogle: [850, 1350, 3000],
+  metrikaYandex: [200, 500, 1000],
+  analyticsGoogle: [350, 750, 1000],
   sendOrder: 500,
   deadlineDay: [
     [2, 7],
@@ -18,31 +18,34 @@ const DATA = {
   deadlinePercent: [20, 17, 15]
 };
 
-const startButton = document.querySelector('.start-button'),
-  firstScreen = document.querySelector('.first-screen'),
-  mainForm = document.querySelector('.main-form'),
-  formCalculate = document.querySelector('.form-calculate'),
-  endButton = document.querySelector('.end-button'),
-  total = document.querySelector('.total'),
-  fastRange = document.querySelector('.fast-range'),
-  totalPriseSum = document.querySelector('.total_price__sum');
+const startButton = document.querySelector(".start-button"),
+  firstScreen = document.querySelector(".first-screen"),
+  mainForm = document.querySelector(".main-form"),
+  formCalculate = document.querySelector(".form-calculate"),
+  endButton = document.querySelector(".end-button"),
+  total = document.querySelector(".total"),
+  fastRange = document.querySelector(".fast-range"),
+  totalPriceSum = document.querySelector(".total_price__sum"),
+  adapt = document.querySelector(".adapt"),
+  adaptMobile = document.querySelector(".adapt_mobile");
 
 // Begin Function
 function showElem(elem) {
-  elem.style.display = 'block';
+  elem.style.display = "block";
 }
 
 function hideElem(elem) {
-  elem.style.display = 'none';
+  elem.style.display = "none";
 }
 
 function priceCalculation(elem) {
   let result = 0,
-    index = 0;
+    index = 0,
+    options = [];
 
-  if (elem.name === 'whichSite') {
+  if (elem.name === "whichSite") {
     for (const item of formCalculate.elements) {
-      if (item.type === 'checkbox') {
+      if (item.type === "checkbox") {
         item.checked = false;
       }
     }
@@ -50,41 +53,70 @@ function priceCalculation(elem) {
   }
 
   for (const item of formCalculate.elements) {
-    if (item.name === 'whichSite' && elem.checked) {
+    if (item.name === "whichSite" && item.checked) {
       index = DATA.whichSite.indexOf(item.value);
-      console.log(index);
+    } else if (item.classList.contains("calc-handler") && item.checked) {
+      options.push(item.value);
     }
   }
-  result = DATA.price[index];
+  options.forEach(function(key) {
+    if (typeof DATA[key] === "number") {
+      if (key === "sendOrder") {
+        result += DATA[key];
+      } else {
+        result += (DATA.price[index] * DATA[key]) / 100;
+      }
+    } else {
+      if (key === "desktopTemplates") {
+        result += (DATA.price[index] * DATA[key][index]) / 100;
+      } else {
+        result += DATA[key][index];
+      }
+    }
+  });
 
-  totalPriseSum.textContent = result;
+  result += DATA.price[index];
+
+  totalPriceSum.textContent = result;
 }
 
 function handlerCallbackForm(event) {
   const target = event.target;
 
-  if (target.classList.contains('want-faster')) {
+  if (target.classList.contains("want-faster")) {
     target.checked ? showElem(fastRange) : hideElem(fastRange);
   }
 
-  if (target.classList.contains('calc-handler')) {
+  if (target.classList.contains("calc-handler")) {
     priceCalculation(target);
   }
 }
+
+function adaptChecker(event) {
+  if (event.target.checked === true) {
+    adaptMobile.disabled = false;
+  } else {
+    adaptMobile.disabled = true;
+    adaptMobile.checked = false;
+  }
+}
+
 // End Function
 
-startButton.addEventListener('click', function () {
+startButton.addEventListener("click", function() {
   showElem(mainForm);
   hideElem(firstScreen);
 });
 
-endButton.addEventListener('click', function () {
+endButton.addEventListener("click", function() {
   for (const elem of formCalculate.elements) {
-    if (elem.tagName === 'FIELDSET') {
+    if (elem.tagName === "FIELDSET") {
       hideElem(elem);
     }
   }
   showElem(total);
 });
 
-formCalculate.addEventListener('change', handlerCallbackForm);
+formCalculate.addEventListener("change", handlerCallbackForm);
+
+adapt.addEventListener("change", adaptChecker);
